9977cf0a6a6bb8cfe6d813bf1518a0c0e829a12d
Mirahy <mirahy@gmail.com>
2022-03-07 21:08:16 -0400

    Aula 07.03.2022

--- src/main/java/br/edu/ifms/estoque/dao/ClienteDao.java
+++ src/main/java/br/edu/ifms/estoque/dao/ClienteDao.java
@@ -8,6 +8,8 @@ import br.edu.ifms.estoque.database.Conexao;
 import br.edu.ifms.estoque.model.Cliente;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import org.hibernate.HibernateException;
 
 /**
  *
@@ -24,44 +26,71 @@ public class ClienteDao implements IClienteDao {
     @Override
     public List<Cliente> buscarPorNome(String nome) {
         EntityManager em = getEntityManager();
-        List<Cliente> clientes = em.createQuery(
-                JPQL + " WHERE c.nome LIKE ?1 "
-        ).setParameter(1, "%" + nome + "%")
+        String condicao = "";
+        List<Cliente> clientes = null;
+        Boolean hasNome = nome != null || !nome.isBlank() || !nome.isEmpty();
+        if(hasNome){
+            condicao = "WHERE c.nome LIKE ?1 ";
+        }
+       Query query = em.createQuery(JPQL + condicao);
+       if(hasNome){
+           clientes = query.setParameter(1, "%" + nome + "%")
                 .getResultList();
-        return clientes;
+       }else{
+           clientes = query.getResultList();
+       } 
+       em.close();
+       return clientes;    
     }
 
     @Override
     public Cliente inserir(Cliente object) {
         EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        em.persist(object);
-        em.getTransaction().commit();
+        try {
+            em.getTransaction().begin();
+            em.persist(object);
+            em.getTransaction().commit();
+            
+        } catch (HibernateException ex) {
+            em.getTransaction().rollback();
+        }
+        em.close();
         return object;
     }
 
     @Override
     public Cliente alterar(Cliente object) {
         EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        em.merge(object);
-        em.getTransaction().commit();
-        return object;   
-
+        em.detach(object);
+        try {
+            em.getTransaction().begin();
+            em.merge(object);
+            em.getTransaction().commit();
+            
+        } catch (HibernateException ex) {
+            em.getTransaction().rollback();
+        }
+        em.close();
+        return object;
     }
 
     @Override
     public void excluir(Object object) {
+        Long id =(Long) object;
         EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        em.remove(object);
-        em.getTransaction().commit();
+        try {
+            em.getTransaction().begin();
+            em.remove(em.getReference(Cliente.class, id));
+            em.getTransaction().commit();       
+        } catch (HibernateException ex) {
+            em.getTransaction().rollback();
+        }
+        em.close();
     }
 
     @Override
     public List<Cliente> listar() {
-
-        return null;
+        return buscarPorNome(null);
 
     }
 
@@ -70,6 +99,7 @@ public class ClienteDao implements IClienteDao {
         EntityManager em = getEntityManager();
         Long id = (Long) object;
         Cliente cliente = em.find(Cliente.class, id);
+        em.close();
         return cliente;
     }
 
---
+++ src/main/java/br/edu/ifms/estoque/database/ClienteHibernateTableModel.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.estoque.database;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author aluno
+ */
+public class ClienteHibernateTableModel extends AbstractTableModel{
+    
+    
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getValueAt(int arg0, int arg1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
